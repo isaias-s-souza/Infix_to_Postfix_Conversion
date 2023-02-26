0b27d81b55ff24f2c77d3970907f2ffc2a8ddc1c
Isaias Souza <isaias.souza.spfc@gmail.com>
2023-02-26 17:21:01 -0300

    Implementação de Tabela do Iterações dada cada expressão e geração de arquivo saída JSON

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Infix_to_Postfix_Conversion
 
+## Passo a passo resumido.
+    - Leu um operando. Escreve na saída
+    - Leu abertura de parênteses. Empilha.
+    - Leu fechamento de parênteses. Desempilha escrevendo na saída até encontrar a abertura de parênteses correspondente. Desempilha abertura correspondente sem escrita na saída.
+    - Leu operador, verifica o topo da pilha e faz a comparação entre os dois, desempilhando e escrevendo na saída até esvaziar a pilha caso o operador atualmente lido for de menor prioridade que o topo em cada nova comparação após o desempilhamento. Se o operador lido for de maior prioridade que o topo da pilha, somente será empilhado.
+    - Após a leitura de todos caracteres da expressão será desempilhado todos operadores da pilha.
 ## Defifinição de Expressões Infixadas e Pós-Fixadas
 
 ### Infixada
@@ -19,7 +25,7 @@
     
 ### Logo, você deverá escolher uma das opções abaixo:
     DIGITE UMA OPÇÃO 
-    1 - Iserir uma Expressão 
+    1 - Inserir uma Expressão 
     
         1+2-3
         Notação Infixada: 1+2-3  
--- main.py
+++ main.py
@@ -1,49 +1,86 @@
-import os
+import json
 
-Operators = set(['+', '-', '*', '/', '(', ')', '^'])  # collection of Operators
+# Conjuntos de operadores
+Operators = {'+', '-', '*', '/', '(', ')', '^'}  
 
-Priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}  # dictionary having priorities of Operators
+# Dicionário contendo a pontuação de prioridades dos operadores
+Priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}  
 
 # https://favtutor.com/blogs/infix-to-postfix-conversion#:~:text=How%20to%20Convert%20Infix%20to,match%20the%20order%20of%20operation.
 
 def infixToPostfix(expression):
-    stack = []  # initialization of empty stack
-
-    output = ''
+    stack                   = []  # Declaração de uma "pilha" vazia
+    output                  = '' # Declara a saída vazia                                                                                                                                                                                                                    
+    step                    = 1
+    operation_stack         = ''
+    unstaking_operating     = False
+    total_output            = dict()
+    total_output['steps']   = list()
+    steps                   = dict() 
 
     for character in expression:
-
-        if character not in Operators and character.isdigit():  # if an operand append in postfix expression
-
+        unstaking_operating = False
+        operation_stack     = ''
+        
+        if character not in Operators:  # Se o caracter for um operando já escreve na saída.
             output += character
-
-        elif character == '(':  # else Operators push onto stack
-
+        elif character == '(':  # Se encontrar abertura: EMPILHA CARACTER
             stack.append('(')
-
-        elif character == ')':
+            operation_stack = "Empilhou abertura '('. "
+        elif character == ')':  # Se encontrar fechamento: 
+                                # DESEMPILHA TODOS CARACTERES EMPILHADOS 
+                                # OU ATÉ ENCONTRAR A PRIMEIRA ABERTURA EMPILHADA
 
             while stack and stack[-1] != '(':
-                output += stack.pop()
+                output              += stack.pop()
+                unstaking_operating = True
 
-            stack.pop()
+            if unstaking_operating:
+                operation_stack = "Desempilhou até antes da abertura '('. "
 
-        else:
+            stack.pop() # Desempilha abertura
+
+        else: # OPERADORES MATEMÁTICOS
             
             while stack and stack[-1] != '(' and Priority[character] <= Priority[stack[-1]]:
                 output += stack.pop()
+                unstaking_operating = True
+            
+            if unstaking_operating:
+                operation_stack = 'Desempilhou até acabar a pilha ou até encontrar no topo um operador com prioridade menor. '
 
             stack.append(character)
+            operation_stack += "Empilhou o caracter '" + character + "'. "
 
+        steps['Passo ' + str(step)] = {
+                                        'current_token' : character,  
+                                        'operation_stack': operation_stack,
+                                        'stack_represetation': ''.join(stack),
+                                        'postfix_expression': output
+                                      }
+        step += 1
+
+    unstaking_operating = False
     while stack:
         output += stack.pop()
+        unstaking_operating = True
+    
+    if unstaking_operating:
+        steps['Passo ' + str(step)] = {
+                                        'current_token' : 'Fim',  
+                                        'operation_stack': 'Desempilhou até a pilha ficar vazia. ',
+                                        'stack_represetation': ''.join(stack),
+                                        'postfix_expression': output
+                                    }
+    total_output['steps'].append(steps)
+    total_output['postfix_expression_final'] = output
 
-    return output
+    return total_output
 
 
 if __name__ == '__main__':
     
-    print("DIGITE UMA OPÇÃO \n1 - Iserir uma Expressão \n2 - Ler Expressões já existentes no arquivo 'input.txt'\n")
+    print("DIGITE UMA OPÇÃO \n1 - Inserir uma Expressão \n2 - Ler Expressões já existentes no arquivo 'input.txt'\n")
     op = int(input())
     if(op == 1):
         expression = input('Entre com a expressão Infixada: ')
@@ -53,10 +90,17 @@ if __name__ == '__main__':
     elif(op==2):
         file_input_expression = open("input.txt", "r")
         list_file_input = file_input_expression.readlines()
-
+        output_postfix = dict()
         for line in list_file_input:
-            print('Notação Infixada: {}Notação Pós-Fixada: {}'.format(line, infixToPostfix(line.strip())))
+            expression = line.strip()
+            operations_postfix = infixToPostfix(expression)
+            output_postfix[expression] = operations_postfix
+            print('Notação Infixada: {}Notação Pós-Fixada: {}'.format(line, operations_postfix['postfix_expression_final']))
             print('\n')
+        
+        with open("output.json", "w", encoding="utf8") as write_file:
+            json.dump(output_postfix, write_file, indent = 4, ensure_ascii= False)
+
         file_input_expression.close()
     else:
         print('Opção Inválida!')
